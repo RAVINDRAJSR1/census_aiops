7c50c980b6689930cd841297d1e01c2d0097a947
ravindra <ravindrajsr@gmail.com>
2022-04-24 09:46:12 +0530

    name changed

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="census-consumer-complaint",
+    name="census-consumer-complaint-rk",
     license="MIT",
-    version="0.0.7",
-    description="Project has been completed.",
-    author="Avnish Yadav",
+    version="0.0.0",
+    description="Project has been start",
+    author="Ravindr Kr",
     packages=find_packages(),
     install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
 )
